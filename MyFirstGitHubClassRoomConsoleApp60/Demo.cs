9c4f90a461c72e9bdfb232598c3bfc4f6603cb5e
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-08-27 10:57:13 +0200

    2 properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+    }
+}
